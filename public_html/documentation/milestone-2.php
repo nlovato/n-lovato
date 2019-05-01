cedadd1faa7b9977f1790e82fcacda1647291a48
Natasha Lovato <nmarshlovato@cnm.edu>
2019-04-30 21:07:19 -0600

    Added headers for each section

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -26,30 +26,30 @@
 	</p>
 
 		<br><hr />
+		<h4><ins>Shaved Ice Flavors:</ins></h4>
 		<p>
-			<ins>Shaved Ice Flavors:</ins>
 			This section will display images of shaved ice and patrons eating shaved ice. This
 			section will also feature a list with a scrollbar.
 		</p>
 
 		<br><hr />
+		<h4><ins>Future Events:</ins></h4>
 		<p>
-			<ins>Future Events:</ins>
 			This section will list and display images of locations or establishment images of
 			upcoming events. Below each image will describe event reason, location, business name/
 			sponsor name if applicable. There will also be a link to <em>Book an Event</em>.
 		</p>
 
 		<br><hr />
+		<h4><ins>Locations:</ins></h4>
 		<p>
-			<ins>Locations:</ins>
 			The <em>Locations</em> section will feature a google map pinning both <em>Rainbow Snow</em>
 			establishments along with images and address below location images.
 		</p>
 
 		<br><hr />
+			<h4><ins>Contact Us:</ins></h4>
 		<p>
-			<ins>Contact Us:</ins>
 			The <em>Contact Us</em> section will feature name, emal and message field. There will be
 			an image with a thank you message. Below will be links to facebook, email, yelp and instagram
 			profiles.
